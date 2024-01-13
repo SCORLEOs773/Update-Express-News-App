500ed6c0197d78e2d959f0a4e468fa94bfe2568e
SUKHAD SHARMA <mykeyman777@gmail.com>
2024-01-13 11:54:01 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import React, { Component } from 'react';
-import Navbar from './Components/Navbar';
-import News from './Components/News';
+import "./App.css";
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: 'light',
-      isLoading: false
+      mode: "light",
+      isLoading: false,
     };
   }
 
@@ -21,59 +22,77 @@ export default class App extends Component {
       <div className={`bg-${this.state.mode}-subtle overflow-hidden`}>
         <Navbar mode={this.state.mode} />
         <hr />
-        <div className="themeTitle position-absolute top-1 text-info end-0" style={{ marginRight: '170px', marginTop: '10px', fontWeight: 'bolder' }}>THEMES</div>
+        <div
+          className="themeTitle position-absolute top-1 text-info end-0"
+          style={{
+            marginRight: "170px",
+            marginTop: "10px",
+            fontWeight: "bolder",
+          }}
+        >
+          THEMES
+        </div>
 
         <div className="position-absolute top-1 end-0 m-5">
           <button
-            className={`btn mx-2 btn-light btn-circle ${this.state.mode === 'light' ? 'active' : ''
-              }`}
-            onClick={() => this.changeTheme('light')}
+            className={`btn mx-2 btn-light btn-circle ${
+              this.state.mode === "light" ? "active" : ""
+            }`}
+            onClick={() => this.changeTheme("light")}
           >
             L
           </button>
 
           <button
-            className={`btn mx-2 btn-dark btn-circle ${this.state.mode === 'dark' ? 'active' : ''
-              }`}
-            onClick={() => this.changeTheme('dark')}
+            className={`btn mx-2 btn-dark btn-circle ${
+              this.state.mode === "dark" ? "active" : ""
+            }`}
+            onClick={() => this.changeTheme("dark")}
           >
             D
           </button>
 
           <button
-            className={`btn mx-2 btn-danger btn-circle ${this.state.mode === 'danger' ? 'active' : ''
-              }`}
-            onClick={() => this.changeTheme('danger')}
+            className={`btn mx-2 btn-danger btn-circle ${
+              this.state.mode === "danger" ? "active" : ""
+            }`}
+            onClick={() => this.changeTheme("danger")}
           >
             R
           </button>
 
           <button
-            className={`btn mx-2 btn-success btn-circle ${this.state.mode === 'success' ? 'active' : ''
-              }`}
-            onClick={() => this.changeTheme('success')}
+            className={`btn mx-2 btn-success btn-circle ${
+              this.state.mode === "success" ? "active" : ""
+            }`}
+            onClick={() => this.changeTheme("success")}
           >
             G
           </button>
 
           <button
-            className={`btn mx-2 btn-warning btn-circle ${this.state.mode === 'warning' ? 'active' : ''
-              }`}
-            onClick={() => this.changeTheme('warning')}
+            className={`btn mx-2 btn-warning btn-circle ${
+              this.state.mode === "warning" ? "active" : ""
+            }`}
+            onClick={() => this.changeTheme("warning")}
           >
             Y
           </button>
 
           <button
-            className={`btn mx-2 btn-primary btn-circle ${this.state.mode === 'primary' ? 'active' : ''
-              }`}
-            onClick={() => this.changeTheme('primary')}
+            className={`btn mx-2 btn-primary btn-circle ${
+              this.state.mode === "primary" ? "active" : ""
+            }`}
+            onClick={() => this.changeTheme("primary")}
           >
             B
           </button>
         </div>
 
-        <News pageSize={12} />
+        <Routes>
+          <Route path="/" element={<News pageSize={12} />} />
+        </Routes>
+        {/* <News pageSize={12} /> */}
       </div>
     );
   }
